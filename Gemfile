eb7aa7f20ee7b2a09df6cae2468d9529d3b759d8
huynv184120 <nguyenviethuy2403@gmail.com>
2022-08-17 10:16:18 +0700

    update
    update

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.1.2"
 gem "bcrypt", "3.1.13"
 gem "bootsnap", ">= 1.4.4", require: false
 gem "bootstrap-sass", "3.4.1"
+gem "config"
 gem "jbuilder", "~> 2.7"
 gem "mysql2", "~> 0.5.4"
 gem "puma", "~> 5.0"
@@ -13,7 +14,6 @@ gem "rails", "~> 6.1.6"
 gem "sass-rails", ">= 6"
 gem "turbolinks", "~> 5"
 gem "webpacker", "~> 5.0"
-gem "config"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
   before_save :downcase_email
-  validates :name, presence: true, length: {maximum: Settings.valid.name.maxlength}
-  validates :email, presence: true, length: {minimum: Settings.valid.email.minlength, maximum: Settings.valid.email.maxlength},
-    format: {with: Settings.regex.VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.valid.password.minlength}, if: :password
+  validates :name, presence: true,
+    length: {maximum: Settings.valid.name.maxlength}
+  validates :email, presence: true,
+    length: {minimum: Settings.valid.email.minlength,
+             maximum: Settings.valid.email.maxlength},
+    format: {with: Settings.regex.VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.valid.password.minlength}, if: :password
   has_secure_password
-  
+
+  private
   def downcase_email
     email.downcase!
   end
