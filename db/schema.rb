6073c74e030d4d9686482840a6873766fb2b9d84
huynv184120 <72036684+huynv184120@users.noreply.github.com>
2022-08-17 13:37:30 +0700

    Revert "chapter6"

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_14_065548) do
-
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
