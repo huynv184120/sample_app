ede82ace4f95d6dad728cffe50fa80270c7feae4
huynv184120 <nguyenviethuy2403@gmail.com>
2022-08-23 09:33:52 +0700

    chapter9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      if params[:session][:remeber_me] == "1"
-        remember user
-      else
-        forget user
-      end
-      redirect_to user
+      handle_login user
     else
       flash.now[:danger] = t(".invalid_pass_or_email")
       render :new
@@ -20,5 +14,17 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_path
+  end
+
+  private
+
+  def handle_login user
+    log_in user
+    if params[:session][:remeber_me] == "1"
+      remember user
+    else
+      forget user
+    end
+    redirect_to user
   end
 end
